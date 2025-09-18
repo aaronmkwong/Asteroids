20bd30755438551fba4b93d7d951b3f35a014911
aaronmkwong <amk841@gmail.com>
2025-09-18 16:46:14 -0600

    A: add circle shape file and frames per second in main

---
+++ circleshape.py
@@ -0,0 +1,24 @@
+import pygame
+
+# base class for game objects
+# extends sprite class to also store a position, velocity, radius
+# player is triangle with non-visible circle to detect collisions
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
--- main.py
+++ main.py
@@ -2,17 +2,32 @@ import pygame
 from  constants import *
 
 def main():
+    
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # get new GIU window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # intentional infinite loop
     while True:
+
+        # break when window closes  
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
+        
+        screen.fill("black") # solid blank screen fill
+        
+        pygame.display.flip() # refresh screen
 
+        dt = clock.tick(60) / 1000 # pause 1/60 second and return delta time in seconds
+
+# main() function only called when file directly run
 if __name__ == "__main__":
     main()
